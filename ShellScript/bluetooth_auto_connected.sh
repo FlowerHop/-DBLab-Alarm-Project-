07ca6d82d28e618140dcc2d2aa172ee46c0cf616
FlowerHop <l941166@hotmail.com>
2016-04-11 16:30:39 +0800

    new /ShellScript/device_list.txt configure file and implement the feature of automaticall scanning and connecting based on the configure file

--- ShellScript/bluetooth_auto_connected.sh
+++ ShellScript/bluetooth_auto_connected.sh
@@ -1,19 +1,48 @@
 #! /bin/bash
-hc05=”30:15:01:09:02:78”
-scannedStr=$(hcitool scan)
-scannedDevice=${scannedStr:14:${#scannedStr}}
-scannedDeviceFile=./ScannedDeviceListTempFile.txt
 
-echo “Find this ${hc05}”
-echo “Scanned Device List ${scannedDevice}”
-echo ${scannedDevice} > ${scannedDeviceFile}
+# the configure file of the device list
+deviceListFile=./device_list.txt
 
-finalStr=$(grep “${hc05}” ./ScannedDeviceListTempFile.txt)
+# read the device list
+exec < $deviceListFile
+read deviceListData
 
-if [ “${finalStr}” != “” ]; then
-  echo “Find ${hc05}”
-  echo “Start to connect...”
-  sudo rfcomm connect /dev/rfcomm0 ${hc05} 1
-else
-  echo “No Result!”
-fi
+deviceList=$(echo $deviceListData | tr "," " ")
+
+# display all the devices in the configure
+echo "Device list: "
+for device in $deviceList
+do
+  echo "  $device"
+done
+
+# Maybe it will need another configure file to set the interval of others frequency to overcome the low battery life.
+# Now, intervall of scanning is 30s.
+## loop for scan and connect 
+while true 
+do
+  scannedStr=$(hcitool scan)
+  scannedDevice=${scannedStr:14:${#scannedStr}}
+  scannedDeviceFile=./ScannedDeviceListTempFile.txt
+
+  for device in $deviceList
+  do
+    echo “Find this ${device}”
+    echo “Scanned Device List ${scannedDevice}”
+    echo ${scannedDevice} > ${scannedDeviceFile}
+
+    finalStr=$(grep “${device}” ./ScannedDeviceListTempFile.txt)
+
+    if [ “${finalStr}” != “” ]; then
+      echo “Find ${device}”
+      echo “Start to connect...”
+      sudo rfcomm connect /dev/rfcomm0 ${device} 1
+    else
+      echo “No Result!”
+    fi
+  done
+
+  sleep 30s
+done
+
+
