b4e382c590333ec710633d71af462b6f538dad28
FlowerHop <l941166@hotmail.com>
2016-05-12 22:57:30 +0800

    implement rssi to know which hop the bio watches are closer

--- Server/server.js
+++ Server/server.js
@@ -30,20 +30,19 @@ app.use(function(req, res, next) {
     next();
 });
 
-
-//var places = ["Room A", "Room B", "Room C", "Restroom A", "None"];
-
-app.get ('/test/update_status/:inPlace/:bioWatchId/:pulse/:dateAndTime', function (req, res) {
+app.get ('/test/update_status/:inPlace/:bioWatchId/:pulse/:dateAndTime/:rssi', function (req, res) {
   var inPlace = req.params.inPlace;
   var bioWatchId = req.params.bioWatchId;
   var pulse = req.params.pulse;
   var dateAndTime = req.params.dateAndTime;
+  var rssi = req.params.rssi;
 
   var bioWatchSignal = {
     inPlace: inPlace,
     bioWatchId: bioWatchId, 
     pulse: pulse,
-    dateAndTime: dateAndTime
+    dateAndTime: dateAndTime,
+    rssi: rssi
   };
 
   var options = {
@@ -77,6 +76,7 @@ app.post ('/api/patients_status', function (req, res) {
     var inPlace = req.body.inPlace;
     var bioWatchId = req.body.bioWatchId;
     var pulse = req.body.pulse;
+    var rssi = req.body.rssi;
 
     // find the place of this bio watch
     var lastPlace = -1;
@@ -91,6 +91,7 @@ app.post ('/api/patients_status', function (req, res) {
           // in the same place
           if (lastPlace === inPlace) {
             devices[j].pulse = pulse;
+            devices[j].rssi = rssi;
           } else {
             // remove the last place of the bio watch
             devices.splice (j,1);
@@ -98,7 +99,7 @@ app.post ('/api/patients_status', function (req, res) {
             // change place
             for (var k = 0; k < patients_status.length; k++) {
               if (patients_status[k].inPlace === inPlace) {
-                patients_status[k].devices.push ({device_id: bioWatchId, pulse: pulse});
+                patients_status[k].devices.push ({device_id: bioWatchId, rssi: rssi, pulse: pulse});
                 break;
               }
             }
@@ -116,7 +117,7 @@ app.post ('/api/patients_status', function (req, res) {
     if (lastPlace === -1) {
       for (var i = 0; i < patients_status.length; i++) {
         if (patients_status[i].inPlace === inPlace) {
-          patients_status[i].devices.push ({device_id: bioWatchId, pulse: pulse});          
+          patients_status[i].devices.push ({device_id: bioWatchId, rssi: rssi, pulse: pulse});          
           break;
         }
       }
@@ -153,6 +154,102 @@ app.get ('/test/showRecords', function (req, res) {
   res.end ();
 });
 
+app.get ('/test/scanedResult/:inPlace/:bioWatchId/:rssi/', function (req, res) {
+  var inPlace = req.params.inPlace;
+  var bioWatchId = req.params.bioWatchId;
+  var rssi = req.params.rssi;
+
+  var bioWatchSignal = {
+    inPlace: inPlace,
+    bioWatchId: bioWatchId, 
+    rssi: rssi
+  };
+
+  var options = {
+    url: 'http://localhost:' + app.get ('port') + '/api/scanedResult',
+    method: 'POST',
+    json: true,
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: bioWatchSignal
+  };
+
+  request (options, function (error, response, body) {
+    if (error) {
+      console.log ('Post data error: ' + error);
+    }
+    console.log (response.body);
+  }); 
+
+  res.end ();
+});
+
+app.post ('/api/scanedResult', function (req, res) {
+  // I would check if the last rssi is smaller (the last is farther) then connect(send: 1) else nothing(send: 0)
+  var toConnect = '-1';
+  
+  fs.readFile (PATIENTS_STATUS_FILE, function (err, data) {
+    if (err) {
+      console.log ("Error: " + error);
+      return;
+    }
+    
+    var patients_status = JSON.parse (data);
+ 
+    var inPlace = req.body.inPlace;
+    var bioWatchId = req.body.bioWatchId;
+    var rssi = req.body.rssi;
+    
+    // find the place of this bio watch
+    var foundPlace = -1;
+    for (var i = 0; i < patients_status.length; i++) {
+      var devices = patients_status[i].devices;
+
+      for (var j = 0; j < devices.length; j++) {
+        // find the bio watch whether exist
+        if (devices[j].device_id === bioWatchId) {
+          foundPlace = i;
+
+          // if it doesn't connect yet
+          if (patients_status[foundPlace].inPlace == 'None') {
+            toConnect = '1';
+            res.send (toConnect);
+            break;
+          } 
+
+          if (foundPlace === inPlace) {
+            devices[j].rssi = rssi;
+            toConnect = '0';
+          } else {
+            // to check
+            if (parseInt(devices[j].rssi) < parseInt(rssi)) {
+              toConnect = '1';
+            } else {
+              toConnect = '0';
+            }
+          }
+          res.send (toConnect);
+          break;
+        }
+      }
+
+      if (foundPlace != -1) {
+        break;
+      }
+    }
+   
+    fs.writeFile (PATIENTS_STATUS_FILE, JSON.stringify(patients_status), function(err) {
+      if (err) {
+        console.error ("Error: " + err);
+        return;
+      }
+    });
+
+    res.end ();
+  });
+});
+
 app.listen (app.get ('port'), function () {
   console.log ('Ready on port: ' + app.get ('port'));
   initialStatus ();
@@ -179,7 +276,7 @@ function initialStatus () {
     var None = {inPlace: 'None', devices: []};
     for (var i = 0; i < bioWatches.length; i++) {
       var bioWatch = bioWatches[i];
-      None.devices.push ({device_id: bioWatch, pulse: 0});
+      None.devices.push ({device_id: bioWatch, rssi: 0, pulse: 0});
     }
 
     initial_status.push (None);
